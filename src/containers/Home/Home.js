644eb780bb36d91c6f112875b275817b289c1bd9
Cristofer Flores <cristofer.flort@gmail.com>
2018-05-05 20:00:28 -0500

    Now the pdf is been rendered, added a few documentation to the code

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { connect } from 'react-redux';
 
 import * as homeActions from '../../store/actions';
 import checkvalidity from '../../utils/validation';
+import * as jsPDF from 'jspdf'
+import * as canvg from 'canvg/canvg';
 
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 import {
   Step,
   StepLabel,
@@ -18,6 +21,7 @@ import FontIcon from 'material-ui/FontIcon';
 
 import VerticalStepper from '../../components/UI/VerticalStepper/VerticalStepper';
 import Diploma from '../../components/UI/Diploma/Diploma';
+
 
 class Home extends Component {
   state = {
@@ -47,7 +51,6 @@ class Home extends Component {
     this.props.onInit();
   }
   
-
   handleNextStep = () => {
     const {stepIndex} = this.state;
     this.setState({
@@ -67,10 +70,35 @@ class Home extends Component {
     }
   };
 
+
+  /**
+  * @description This method create a pdf of a React Component
+  *     1.- Create a canvas element so we can put our react component in there
+  *     2.- Using canvg we create a canvas based on a svg that's why we call 'renderToStaticMarkup'
+  *     3.- Get data url of the canvas
+  *     4.- Render the canvas with jsPDF
+  */
   createPdf = () => {
-    console.log('state', this.state);
+    const {stepIndex} = this.state;
+    let canvas = document.createElement('canvas');
+    canvg(canvas, ReactDOMServer.renderToStaticMarkup(<Diploma name={this.state.form.user.props.name} />), {});
+    let imgData = canvas.toDataURL('image/png'), 
+        doc = new jsPDF('l', 'pt', 'a4'),
+        filename = `Contancia - ${this.state.form.user.props.name.replace(/[\. ,:-]+/g, '-')}`;
+
+    doc.addImage(imgData, 'PNG', 0, 0, canvas.width * .53, canvas.height * .49);
+    doc.save(filename);
+    this.setState({
+      ...this.state,
+      stepIndex: stepIndex + 1,
+      finished: true
+    });
   }
 
+  /**
+  * @description We render the different action buttons depending on the step.
+  * @param{string} step Current step on the stepper.
+  */
   renderStepActions(step) {
     const {stepIndex} = this.state;
     const action = stepIndex === 2 ? this.createPdf : this.handleNextStep
@@ -145,10 +173,27 @@ class Home extends Component {
 
   resetValues = () => {
     this.setState({
-      ...this.state,
       finished: false,
       stepIndex: 0,
+      form: {
+        user: {
+          props: {
+            id: null,
+            name: ''
+          },
+          validation: { required: true }
+        },
+        event: {
+          props: {
+            id: null,
+            name: '',
+            users: []
+          },
+          validation: { required: true }
+        }
+      }
     })
+    this.props.onSetUser(null);
   }
 
   render() {
@@ -171,7 +216,7 @@ class Home extends Component {
               />
               <CardText>
                 <div>
-                  <VerticalStepper stepIndex={this.state.stepIndex} finished={this.state.finished} onReset={this.resetValues}>
+                  <VerticalStepper stepIndex={this.state.stepIndex} finished={this.state.finished}>
 
                     <Step>
                       <StepLabel>Selecciona el evento al que asististe:</StepLabel>
@@ -214,8 +259,11 @@ class Home extends Component {
                         {this.renderStepActions(2)}
                       </StepContent>
                     </Step>
-
                   </VerticalStepper>
+                  {this.state.finished && (<div style={{ margin: "20px 0", textAlign: "center"}}>
+                    <h2>Gracias por asistir!</h2>
+                    <FlatButton label="Descargar otr a contancia" fullWidth onClick={this.resetValues}/>
+                  </div>)}
                 </div>
               </CardText>
             </Card>
@@ -225,6 +273,7 @@ class Home extends Component {
     )
   }
 }
+
 const mapStateToProps = (state) => ({
   events: state.events,
   user: state.user,
